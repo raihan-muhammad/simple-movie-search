f78c44c8c384f9feaf0d748df1498c89afd49dc5
raihan-muhammad <sukasukangoding@gmail.com>
2024-01-29 22:48:49 +0700

    feat: create star rating component

---
+++ src/StarRating.jsx
@@ -0,0 +1,30 @@
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+function StarRating({ maxRating = 5 }) {
+  return (
+    <section style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p style={textStyle}>10</p>
+    </section>
+  );
+}
+
+export default StarRating;

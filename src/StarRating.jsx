5f46942c76addf14c74b9e191d209888b6fb6cf5
raihan-muhammad <sukasukangoding@gmail.com>
2024-01-29 23:04:52 +0700

    feat: create stars component

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import Star from "./Star";
+
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -6,7 +9,6 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
-  gap: "4px",
 };
 
 const textStyle = {
@@ -15,14 +17,23 @@ const textStyle = {
 };
 
 function StarRating({ maxRating = 5 }) {
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (rating) => {
+    setRating(rating);
+  };
   return (
     <section style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span>S{i + 1}</span>
+          <Star
+            key={i}
+            onRate={() => setRating(i + 1)}
+            full={rating >= i + 1}
+          />
         ))}
       </div>
-      <p style={textStyle}>10</p>
+      <p style={textStyle}>{rating || ""}</p>
     </section>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import StarRating from "./StarRating";
+// import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating />
   </React.StrictMode>
 );
